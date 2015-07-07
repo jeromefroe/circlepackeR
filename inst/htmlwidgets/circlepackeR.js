6f19c49b4a0b9dc7226cefc77f90681e504a36f8
jeromefroe <jeromefroelich@hotmail.com>
2015-07-07 08:44:53 -0400

    Changed function to zoomable circle packing

--- inst/htmlwidgets/circlepackeR.js
+++ inst/htmlwidgets/circlepackeR.js
@@ -7,58 +7,89 @@ HTMLWidgets.widget({
   initialize: function(el, width, height) {
 
     return {
-      // TODO: add instance fields as required
+
     }
 
   },
 
   renderValue: function(el, x, instance) {
 
-    var diameter = 960,
-    format = d3.format(",d");
+    var margin = 20,
+    diameter = 960;
+
+    var color = d3.scale.linear()
+        .domain([-1, 5])
+        .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+        .interpolate(d3.interpolateHcl);
 
     var pack = d3.layout.pack()
-        .size([diameter - 4, diameter - 4])
-        .value(function(d) { return d.size; });
+        .padding(2)
+        .size([diameter - margin, diameter - margin])
+        .value(function(d) { return d.size; })
 
     var svg = d3.select("body").append("svg")
         .attr("width", diameter)
         .attr("height", diameter)
       .append("g")
-        .attr("transform", "translate(2,2)");
+        .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
 
-//    d3.json(x.path, function(error, root) {
-//      if (error) throw error;
+    function update(root) {
+      var focus = root,
+          nodes = pack.nodes(root),
+          view;
 
-//    JSON.parse(x.data, function(error, root) {
-//      if (error) throw error;
+      var circle = svg.selectAll("circle")
+          .data(nodes)
+        .enter().append("circle")
+          .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
+          .style("fill", function(d) { return d.children ? color(d.depth) : null; })
+          .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
 
-    var root = JSON.parse(x.data)
+      var text = svg.selectAll("text")
+          .data(nodes)
+        .enter().append("text")
+          .attr("class", "label")
+          .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
+          .style("display", function(d) { return d.parent === root ? null : "none"; })
+          .text(function(d) { return d.name; });
 
-    function makeCircles(root) {
+      var node = svg.selectAll("circle,text");
 
-      var node = svg.datum(root).selectAll(".node")
-          .data(pack.nodes)
-        .enter().append("g")
-          .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
-          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+      d3.select("body")
+          .style("background", color(-1))
+          .on("click", function() { zoom(root); });
 
-      node.append("title")
-          .text(function(d) { return d.name + (d.children ? "" : ": " + format(d.size)); });
+      zoomTo([root.x, root.y, root.r * 2 + margin]);
 
-      node.append("circle")
-          .attr("r", function(d) { return d.r; });
+      function zoom(d) {
+        var focus0 = focus; focus = d;
 
-      node.filter(function(d) { return !d.children; }).append("text")
-          .attr("dy", ".3em")
-          .style("text-anchor", "middle")
-          .text(function(d) { return d.name.substring(0, d.r / 3); });
+        var transition = d3.transition()
+            .duration(d3.event.altKey ? 7500 : 750)
+            .tween("zoom", function(d) {
+              var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
+              return function(t) { zoomTo(i(t)); };
+            });
+
+        transition.selectAll("text")
+          .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+            .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
+            .each("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+            .each("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+      }
+
+      function zoomTo(v) {
+        var k = diameter / v[2]; view = v;
+        node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
+        circle.attr("r", function(d) { return d.r * k; });
+      }
     }
-//    );
 
-    makeCircles(root)
+  var root = JSON.parse(x.data);
 
-    d3.select(self.frameElement).style("height", diameter + "px");
+  update(root);
+
+  d3.select(self.frameElement).style("height", diameter + "px");
 
   },
 
