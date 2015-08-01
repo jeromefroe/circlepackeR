ed857f30856028b822bd7b424b32b3cefb19c1de
576163 <jeromefroelich@C02MH24QFD59.local>
2015-08-01 13:16:20 -0400

    Can now accept hierarchical lists

--- R/circlepackeR.R
+++ R/circlepackeR.R
@@ -1,19 +1,24 @@
-#' <Add Title>
+#' htmlwidget for d3.js zoomable circle packing visualizations
 #'
 #' <Add Description>
 #'
 #' @import htmlwidgets
 #'
 #' @export
-circlepackeR <- function(data,
-                         width = NULL, height = NULL) {
+circlepackeR <- function(data, width = NULL, height = NULL) {
 
-  # accept JSON string
-  data = jsonlite::toJSON(
-    jsonlite::fromJSON( data )
-    , auto_unbox = TRUE
-    , dataframe = "rows"
-  )
+  # accept JSON
+  if (inherits(data, c("character", "connection", "json"))) {
+    data = jsonlite::toJSON(
+      jsonlite::fromJSON(data),
+      auto_unbox = TRUE,
+      dataframe = "rows"
+    )
+  } else if (inherits(data, "list")) {  # accept hierarchical list
+    data = jsonlite::toJSON(data, auto_unbox = TRUE)
+  } else{
+    stop("Please provide a json object or list", call.=FALSE)
+  }
 
   # create a list that contains the data
   x = list(
